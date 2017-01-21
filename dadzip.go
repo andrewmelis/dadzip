5d0645f99251d1907409053b7527f0744d7c67d2
Andrew Melis <andrewmelis@gmail.com>
2017-01-21 16:12:58 -0600

    read test data from filesystem instead

--- dadzip.go
+++ dadzip.go
@@ -7,6 +7,8 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	_ "os"
+	_ "path/filepath"
 )
 
 func zipHandler(w http.ResponseWriter, r *http.Request) {
@@ -17,6 +19,7 @@ func zipHandler(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 
 	// validate that sent file is zip
+	// for now, assume
 
 	zr, err := zip.NewReader(bytes.NewReader(body), r.ContentLength)
 	if err != nil {
@@ -25,6 +28,25 @@ func zipHandler(w http.ResponseWriter, r *http.Request) {
 
 	for _, f := range zr.File {
 		fmt.Fprintf(w, "%s\n", f.FileHeader.Name)
+
+		// // zipWalkFunc(f
+		// filepath.Walk(f.FileHeader.Name, func(path string, _ os.FileInfo, _ error) error {
+		// 	fmt.Fprintf(w, "%s\n", path)
+		// 	// log.Printf(path)
+		// 	// log.Printf(info.Name())
+		// 	return nil
+		// })
+
 	}
 	// fmt.Fprintf(w, "foobar\n")
 }
+
+// func zipWalk(f zip.File, walkFn zipWalkFunc) error {
+// 	info, err := f.FileHeader.FileInfo()
+// 	if err != nil {
+// 		return walkFn(root, nil, err)
+// 	}
+// 	return walk(root, info, walkFn)
+// }
+
+// type zipWalkFunc func(f zip.File, info os.FileInfo, err error) error
--- dadzip_test.go
+++ dadzip_test.go
@@ -5,11 +5,34 @@ import (
 	"bytes"
 	_ "fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	_ "net/http"
 	"net/http/httptest"
+	_ "os"
 	"testing"
 )
+
+func TestNewSingle(t *testing.T) {
+	b, err := ioutil.ReadFile("testdata/singlefile.zip")
+	if err != nil {
+		t.Errorf("could not open test data")
+	}
+
+	r := bytes.NewReader(b)
+
+	req := httptest.NewRequest("POST", "/filenames", r)
+	w := httptest.NewRecorder()
+
+	zipHandler(w, req)
+
+	expected := "foo.txt\n"
+	actual := w.Body.String()
+
+	if expected != actual {
+		t.Errorf("expected: %s, actual: %s\n", expected, actual)
+	}
+}
 
 func TestSendZipSingleFile(t *testing.T) {
 	testZip := NewTestZip()
