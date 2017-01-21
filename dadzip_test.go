af2d5962540716e2c8bb2a4a9a28eabcb98d2605
Andrew Melis <andrewmelis@gmail.com>
2017-01-21 17:20:07 -0600

    convert to table tests

--- dadzip_test.go
+++ dadzip_test.go
@@ -7,86 +7,34 @@ import (
 	"testing"
 )
 
-func TestZipSingleFile(t *testing.T) {
-	b, err := ioutil.ReadFile("testdata/singlefile.zip")
-	if err != nil {
-		t.Errorf("could not open test data")
-	}
-
-	r := bytes.NewReader(b)
-
-	req := httptest.NewRequest("POST", "/filenames", r)
-	w := httptest.NewRecorder()
-
-	zipHandler(w, req)
-
-	expected := "foo\n"
-	actual := w.Body.String()
-
-	if expected != actual {
-		t.Errorf("expected: %s, actual: %s\n", expected, actual)
-	}
+var ziptests = []struct {
+	testfile string
+	expected string
+}{
+	{"testdata/singlefile.zip", "foo\n"},
+	{"testdata/singledir.zip", "foo\n"},
+	{"testdata/compositenoextensions.zip", "hello\nbar\nreadonly\n"},
+	{"testdata/compositewithextensions.zip", "bar\nhello\nreadonly\n"},
 }
 
-func TestZipSingleDir(t *testing.T) {
-	b, err := ioutil.ReadFile("testdata/singledir.zip")
-	if err != nil {
-		t.Errorf("could not open test data")
-	}
+func TestZipHandler(t *testing.T) {
+	for _, tt := range ziptests {
+		b, err := ioutil.ReadFile(tt.testfile)
+		if err != nil {
+			t.Errorf("could not open test data")
+		}
 
-	r := bytes.NewReader(b)
+		r := bytes.NewReader(b)
 
-	req := httptest.NewRequest("POST", "/filenames", r)
-	w := httptest.NewRecorder()
+		req := httptest.NewRequest("POST", "/filenames", r)
+		w := httptest.NewRecorder()
 
-	zipHandler(w, req)
+		zipHandler(w, req)
 
-	expected := "foo\n"
-	actual := w.Body.String()
+		actual := w.Body.String()
 
-	if expected != actual {
-		t.Errorf("expected: %s, actual: %s\n", expected, actual)
-	}
-}
-
-func TestZipCompositeNoExtensions(t *testing.T) {
-	b, err := ioutil.ReadFile("testdata/compositenoextensions.zip")
-	if err != nil {
-		t.Errorf("could not open test data")
-	}
-
-	r := bytes.NewReader(b)
-
-	req := httptest.NewRequest("POST", "/filenames", r)
-	w := httptest.NewRecorder()
-
-	zipHandler(w, req)
-
-	expected := "hello\nbar\nreadonly\n"
-	actual := w.Body.String()
-
-	if expected != actual {
-		t.Errorf("expected: %s, actual: %s\n", expected, actual)
-	}
-}
-
-func TestZipCompositeWithExtensions(t *testing.T) {
-	b, err := ioutil.ReadFile("testdata/compositewithextensions.zip")
-	if err != nil {
-		t.Errorf("could not open test data")
-	}
-
-	r := bytes.NewReader(b)
-
-	req := httptest.NewRequest("POST", "/filenames", r)
-	w := httptest.NewRecorder()
-
-	zipHandler(w, req)
-
-	expected := "bar\nhello\nreadonly\n"
-	actual := w.Body.String()
-
-	if expected != actual {
-		t.Errorf("expected: %s, actual: %s\n", expected, actual)
+		if tt.expected != actual {
+			t.Errorf("expected: %s, actual: %s\n", tt.expected, actual)
+		}
 	}
 }
