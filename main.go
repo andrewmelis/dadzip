a8f2127523cc9a13afdb3212253bf30fe03ecb79
Andrew Melis <andrewmelis@gmail.com>
2017-01-28 13:50:41 -0600

    Get http server port from ENV

--- main.go
+++ main.go
@@ -3,15 +3,22 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/andrewmelis/dadzip/ziphandler"
 )
 
 func main() {
-	log.Printf("starting server...")
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "./index.html")
 	})
 	http.HandleFunc("/filenames", ziphandler.MultiPartZipHandler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+
+	port, ok := os.LookupEnv("PORT")
+	if !ok {
+		port = "8080"
+	}
+
+	log.Printf("starting server on port %s...", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
